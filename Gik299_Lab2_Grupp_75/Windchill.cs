9df4d8e56ee763047776298510c16b2c46801d3d
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-27 09:35:50 +0100

    Update Windchill.cs
    Test

--- Gik299_Lab2_Grupp_75/Windchill.cs
+++ Gik299_Lab2_Grupp_75/Windchill.cs
@@ -6,7 +6,15 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-
+        // Deklarerar och initierar variabler för att räkna ut vindkyleffekten.
+        // Temperatur och vindhastighet
+        double temperature = 0;
+        double windSpeed = 0;
+        // Vindkyleeffekt
+        double windChill = 0;
+        // Deklarerar och initierar en bool som villkor för while-loopen.
+        bool isRunning = true;
+        // Skapar en
         
     }
 }
